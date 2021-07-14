30a3c6393e28ac49e3bf7bac5ec0e01d15f4babf
Joongoo Lee <ljg4471@gmail.com>
2021-07-14 10:21:29 +0900

    add infer

--- model.py
+++ model.py
@@ -75,7 +75,6 @@ class Models():
         self.models = models
         self.phase = phase
 
-        self.noise = opt.noise
         self.opt = opt
 
         self.logger = logging.getLogger(__name__)
